20591fdd7521facbf73117547d95e53fbfa5df17
cody0623 <aersabes@yahoo.co.jp>
2023-12-12 11:37:21 +0900

    now

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread("画像のパス")
+img = cv2.imread("/home/aersa/wan_wan/kansetsutsuu_ude.png")
 
-print(img)
+print(img.shape)
+
+print(plt.imshow(img))
